f29234aa34d5953383bb7bb8754eea3fa8453683
elfobella <yunusemreucr11@gmail.com>
2024-10-07 21:57:52 +0300

    header and navbar changes - new template

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
-
 export default function App() {
   return (
-    <div className="flex max-w-7xl mx-auto px-4 flex-col gap-10">
-      <Navbar />
-      <Header />
+    <div className="dark:bg-ghost min-h-screen">
+      <div className="flex font-work tracking-tight max-w-7xl mx-auto px-4 flex-col gap-10">
+        <Navbar />
+        <Header />
+      </div>
     </div>
   );
 }
