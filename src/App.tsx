be92367c0cb4b642a341805cdac5e0f31cb01cd1
elfobella <yunusemreucr11@gmail.com>
2024-10-08 15:05:23 +0300

    Navbar,Header done, Dark mode styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./Components/Header";
 import Navbar from "./Components/Navbar";
 export default function App() {
   return (
-    <div className="dark:bg-ghost min-h-screen">
+    <div className="dark:bg-primary min-h-screen">
       <div className="flex font-work tracking-tight max-w-7xl mx-auto px-4 flex-col gap-10">
         <Navbar />
         <Header />
